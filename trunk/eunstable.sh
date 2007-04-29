23ab752ded6e5110a6683a558f12cbb3a2058c50
Tobias Roeser <le.petit.fou@web.de>
2007-04-29 15:01:23 +0000

    Release gentoolkit-lefou-0.6.7
    git-svn-id: file:///home/lefou/work/server-lepetitfou/backup/var-svn/repos/gentoo/gentoolkit-lefou/tags/gentoolkit-lefou-0.6.7@341 ac902c8a-c9f5-0310-8d8e-ff8d2036e5c9

---
+++ trunk/eunstable.sh
@@ -0,0 +1,60 @@
+#!/bin/bash
+#
+# Copyright (c) 2005 - 2007 by Tobias Roeser
+# All rights reserved
+# $Id$
+#
+
+VERSION=0.6.7
+
+# Which backend ?
+GENTOOLKIT_LEFOU_BACKEND="${GENTOOLKIT_LEFOU_BACKEND:-$(source /etc/gentoolkit-lefou.conf 2>/dev/null && echo "${GENTOOLKIT_LEFOU_BACKEND}")}"
+GENTOOLKIT_LEFOU_BACKEND="${GENTOOLKIT_LEFOU_BACKEND:-$(source ${HOME}/.gentoolkit-lefou.conf 2>/dev/null && echo "${GENTOOLKIT_LEFOU_BACKEND}")}"
+
+if [ "${GENTOOLKIT_LEFOU_BACKEND}" = "portage" ] ; then
+        CONF_UNMASK=/etc/portage/package.unmask
+        CONF_KEYWORDS=/etc/portage/package.keywords
+elif [ "${GENTOOLKIT_LEFOU_BACKEND}" = "paludis" ] ; then
+        CONF_UNMASK=/etc/paludis/package_unmask.conf
+        CONF_KEYWORDS=/etc/paludis/keywords.conf
+else
+        echo "No backend defined."
+        exit 1
+fi
+
+# Which arch
+GENTOOLKIT_LEFOU_ARCH="${GENTOOLKIT_LEFOU_ARCH:-$(source /etc/gentoolkit-lefou.conf 2>/dev/null && echo "${GENTOOLKIT_LEFOU_ARCH}")}"
+keyword="~${GENTOOLKIT_LEFOU_ARCH:-x86}"
+
+unmask=false
+unstable=false
+
+if [ "$(basename $0)" == "eunstable" ]; then
+    unstable=true
+elif [ "$(basename $0)" == "eunmask" ]; then
+    unmask=true
+elif [ "$(basename $0)" == "efullunmask" ]; then
+    unmask=true
+    unstable=true
+else
+    echo "!!! Wrong call"
+    echo "!!! this script must be called \"eunstable\", \"eunmask\" or \"efullunmask\""
+    echo "!!! Abort"
+    exit 1
+fi
+
+for package in $*; do
+    
+    if [ $unmask == true ]; then
+	echo ">>> Adding \"=$package\" to ${CONF_UNMASK}"
+	cp ${CONF_UNMASK} ${CONF_UNMASK}.eunmask
+	echo "=$package" >> ${CONF_UNMASK}
+    fi
+
+    if [ $unstable == true ]; then
+	echo ">>> Adding \"=$package $keyword\" to ${CONF_KEYWORDS}"
+	cp ${CONF_KEYWORDS} ${CONF_KEYWORDS}.eunstable
+	echo "=$package $keyword" >> ${CONF_KEYWORDS}
+    fi
+
+done
